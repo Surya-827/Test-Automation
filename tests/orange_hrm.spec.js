78f38e48127811158326c6a0c561bca54e124ee4
Surya-0827 <vsuryakiran827@gmail.com>
2025-06-30 00:31:10 +0530

    Added Fix for Failure Lines in Orange HRM

--- tests/orange_hrm.spec.js
+++ tests/orange_hrm.spec.js
@@ -1,8 +1,11 @@
-import {test,expect} from '@playwright/test';   
+import { test, expect } from '@playwright/test';
 
-test('Organe HRM Login Demo', async ({page}) => {
-    //Navigate to Orange HRM Website 
+test('Organe HRM Login Demo', async ({ page }) => {
+    // Navigate to Orange HRM Website 
     await page.goto("https://opensource-demo.orangehrmlive.com/");
-    await page.pause(1000); // Wait for 1 second to ensure the page is fully loaded
-    await page.locator("//input[@name='username']").click();
-})
+    await page.waitForTimeout(1000); // Wait for 1 second to ensure the page is fully loaded
+    await page.locator("//input[@name='username']").fill("Admin");
+    await page.locator("//input[@name='password']").fill("admin123");
+    await page.waitForSelector("//button[@type='submit']", { timeout: 5000 }); // Correct usage
+    await page.locator("//button[@type='submit']").click();
+});
